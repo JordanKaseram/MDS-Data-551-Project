45ba263a970df2df5f21899b1a37dd71b0c75ad7
JordanKaseram <kaseramj@mymacewan.ca>
2026-02-24 10:41:54 -0800

    Add milestone2 dashboard files

---
+++ milestone2/data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+df_raw = pd.read_csv("data/SuperStoreOrders.csv")
+df = df_raw.copy()
+df["sales"] = (
+    df["sales"]
+        .astype(str)
+        .str.replace(",", "", regex=False)
+        .str.strip()
+)
+
+df["sales"] = pd.to_numeric(df["sales"], errors="coerce")
+df["margin"] = df["profit"] / df["sales"]
+
+df["order_date"] = pd.to_datetime(df["order_date"], format="mixed", dayfirst=True, errors="coerce")
+df["ship_date"]  = pd.to_datetime(df["ship_date"],  format="mixed", dayfirst=True, errors="coerce")
+df["shipping_delay"] = (df["ship_date"] - df["order_date"]).dt.days
+
+
+
